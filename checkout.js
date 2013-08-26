f23b09c99e4ec89c785752841a11a5473832c4b6
David Kemp <drkemp@google.com>
2013-08-26 15:13:10 -0400

    fixed release tagging

--- checkout.js
+++ checkout.js
@@ -7,6 +7,7 @@ var argv = require('optimist').argv;
 var CAT='TOOLS';
 var isRelease=false;
 var jsonpath ='./repos.json';
+var branch_release = 'release';
 
 function getDir(reponame) {
    return path.basename(reponame,'.git');
@@ -15,6 +16,7 @@ function getDir(reponame) {
 if(argv.cat) CAT=argv.cat;
 if(argv.release) isRelease=true;
 if(argv.path) jsonpath=argv.path;
+if(argv.releasebranch) branch_release= argv.releasebranch;
 
 var repos = JSON.parse(fs.readFileSync(jsonpath, 'utf8'));
 
@@ -23,6 +25,7 @@ repos.repos.forEach( function(repo) {
     if(repo.category == CAT) {
         var branch = repo.current;
         if(isRelease) branch = repo.release;
+        if(branch =="RELEASE") branch = branch_release;
         var dir = getDir(repo.repo);
         if(fs.existsSync(dir) && fs.statSync(dir).isDirectory() ) {
             shell.pushd(dir);
