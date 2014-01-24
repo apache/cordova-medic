bd0c9ff8421590d10b1c17dd45d75143cd5d40be
David Kemp <drkemp@google.com>
2014-01-24 16:19:28 -0500

    adding new test runner

---
+++ runner/testrunner.js
@@ -0,0 +1,71 @@
+var http = require('http');
+var path = require ('path');
+var shell = require('shelljs');
+var fs = require('fs');
+var cp = require('child_process');
+var argv = require('optimist').argv;
+var url=require('url');
+
+var logport =0;
+var logurl='http://127.0.0.1';
+var testprocess;
+
+var testpath = process.cwd();
+if(argv.path) testpath=argv.path;
+
+var cmdpath = "./runtest.sh";
+if(argv.cmd) argv.cmd;
+
+function writejson(port,cfgpath){
+  var cfgobj = {logurl:"http://127.0.0.1:"+port};
+  if(!fs.existsSync(cfgpath)) fs.mkdirSync(cfgpath);
+  fs.writeFileSync(path.join(cfgpath,'medic.json'), JSON.stringify(cfgobj));
+}
+
+function startTest(){
+  console.log("starting test "+cmdpath);
+  testprocess=cp.spawn(cmdpath);
+  if(testprocess){
+    console.log("started test: "+testprocess.pid);
+  } else {
+    console.log("failed to start test in "+testpath);
+  }
+}
+
+function endTest(){
+  console.log("ending test.");
+  server.close();
+  if(testprocess){
+    process.kill(testprocess);
+    console.log("killed test.");
+  } else {
+    console.log("cant kill test.");
+  }
+//  process.exit(0);
+}
+
+var server = http.createServer(function (req, res) {
+  if(req.method==="POST") {
+    var route=url.parse(req.url).pathname;
+    var body = '';
+    req.setEncoding('utf8');
+    req.on('data',function(chunk){body += chunk;});
+    req.on('end',function(){
+      console.log(body);
+      if(route == '/result'){
+        endTest();
+      }
+    });    
+    res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin':'*'});
+    res.end('Got that\n');
+  }
+});
+server.listen(logport, '127.0.0.1',511,function(){
+  logport = server.address().port;
+  console.log('Server running at '+logurl+':'+logport);
+  writejson(logport,testpath);
+  startTest();
+});
+
+
+
