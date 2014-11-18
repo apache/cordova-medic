4767f52d4f29f9ea42846331a256defe0835401b
maria.bukharina <maria.bukharina@akvelon.com>
2014-11-18 18:03:44 +0300

    CB-8033 Replaced "coho repo-clone" step by downloading repos  from custom urls from repos.json
    github: close #19

--- checkout.js
+++ checkout.js
@@ -13,47 +13,25 @@ function getDir(reponame) {
    return path.basename(reponame,'.git');
 }
 
-function hasRemote (remote) {
-    get_remote = shell.exec('git remote -v');
-    if (get_remote.code > 0) {
-        return false;
-    }
-    var remotes = get_remote.output.split('\n');
-    for (var i in remotes){
-        if (remotes[i].indexOf(remote) > -1){
-            return true;
-        }
-    }
-    return false;
-}
-
-if(argv.cat) CAT=argv.cat;
-if(argv.release) isRelease=true;
-if(argv.path) jsonpath=argv.path;
-if(argv.releasebranch) branch_release= argv.releasebranch;
+if(argv.cat) CAT = argv.cat;
+if(argv.release) isRelease = true;
+if(argv.path) jsonpath = argv.path;
+if(argv.releasebranch) branch_release = argv.releasebranch;
 
 var repos = JSON.parse(fs.readFileSync(jsonpath, 'utf8'));
 
 repos.repos.forEach( function(repo) {
-    // console.log('checking '+JSON.stringify(repo));
-    if(repo.category == CAT) {
+    if(repo.category == CAT.toUpperCase()) {
         var branch = isRelease ? repo.release : repo.current;
-        branch = branch =="RELEASE" ? branch_release : branch;
+        branch = branch == "RELEASE" ? branch_release : branch;
         var dir = getDir(repo.repo);
         var remotename = path.basename(path.dirname(repo.repo));
-        if(fs.existsSync(dir) && fs.statSync(dir).isDirectory() ) {
-            shell.pushd(dir);
-            if(fs.existsSync('.git')) {
-                // Check if remote from repos.json already added to local repo
-                if (!hasRemote(repo.repo)){
-                    // If not, then add it, and checkout branch from there
-                    console.log('Adding remote: ' + remotename + ' ' + repo.repo);
-                    var remote_add = shell.exec('git remote add -f ' + remotename + ' ' + repo.repo + ' -t ' + branch);
-                }
-                console.log('Checking branch ' + branch);
-                var checkout = shell.exec('git checkout ' + branch);
-            }
-            shell.popd();
+
+        if(fs.existsSync(dir + '/.git')) {
+            console.log('Repo ' + repo.repo + ' has already cloned!');
+            process.exit(1);
+        } else {
+            shell.exec('git clone ' + repo.repo + ' --branch=' + branch + ' --depth ' + '1');
         }
     }
 });
