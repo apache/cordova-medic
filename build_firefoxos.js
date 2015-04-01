b5bb433db3b2e15206ea33637e960c930012905e
Willy Aguirre <marti1125@gmail.com>
2015-03-31 23:08:04 -0500

    add createMedicJson

--- build_firefoxos.js
+++ build_firefoxos.js
@@ -1,26 +1,31 @@
-var path = require ('path');
-var shell = require('shelljs');
-var buildinfo = require('./buildinfo');
-var config = require('./config');
-var firefoxos  = require('./src/build/makers/firefoxos');
-var argv = require('optimist').argv;
-var testcheck = require('./testchecker');
+var path             = require ('path'),
+    buildinfo        = require('./buildinfo'),
+    config           = require('./config'),
+    firefoxos        = require('./src/build/makers/firefoxos'),
+    argv             = require('optimist').argv,
+    testcheck        = require('./testchecker'),
+	createMedicJson  = require('./src/utils/createMedicJson');
+
 
 // this assumes that you start it in the sandbox
-var TEST_DIR=process.cwd();
-var BRANCH='master';
-var MSPEC_DIR=path.join(TEST_DIR,'mobilespec');
+var test_dir   = process.cwd(),
+    branch     = 'master',
+    mspec_dir  = path.join(test_dir,'mobilespec'),
+    TEST_OK    = true;
 
-var TEST_OK=true;
+if(argv.branch) { 
+	branch=argv.branch;
+}
 
-if(argv.branch) BRANCH=argv.branch;
+var output_location = path.join(mspec_dir,'platforms','firefoxos');
 
-var output_location = path.join(MSPEC_DIR,'platforms','firefoxos');
-
-buildinfo('FirefoxOS', BRANCH, function (error, sha ) {
+buildinfo('FirefoxOS', branch, function (error, sha ) {
     if(error) {
         TEST_OK=false;
     } else {
+    	// add medic configuration (sha, host) to destination folder
+        createMedicJson(path.join(mspec_dir, 'www'), sha, config);    	
+
         firefoxos(output_location, sha,'', config.app.entry, config.couchdb, function(err){
             if(err) {
                 console.log('FirefoxOS test prepare failed');
@@ -34,6 +39,8 @@ buildinfo('FirefoxOS', BRANCH, function (error, sha ) {
 });
 
 process.once('exit', function () {
-    if(!TEST_OK) process.exit(1);
+    if(!TEST_OK) {
+    	process.exit(1);
+    }
 });
 
